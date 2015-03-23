7dbc04fd7423dde9de4809bb39a55dcb69e1a648
Andrew Stanford-Jason <andrew@xmos.com>
2015-03-23 16:58:28 +0000

    fixed some of the i2s.h build problems - doxygen

--- lib_i2s/api/i2s.h
+++ lib_i2s/api/i2s.h
@@ -78,7 +78,7 @@ typedef interface i2s_callback_if {
  *  The component performs I2S master so will drive the word clock and
  *  bit clock lines.
  *
- *  \param i              The I2S callback interface to connect to
+ *  \param i2s_i          The I2S callback interface to connect to
  *                        the application
  *  \param p_dout         An array of data output ports
  *  \param num_out        The number of output data ports
@@ -111,10 +111,6 @@ typedef interface i2s_slave_callback_if {
    *   The I2S component will call this
    *   when it first initializes on first run of after a restart.
    *
-   *   \param mclk_bclk_ratio          This should be set to the desired master
-   *                                   clock to bit clock ratio(must be a power
-   *                                   of two thats greater than or equal to two
-   *                                   and less than or equal to 32.
    *   \param mode                     The transfer mode (I2S_MODE_I2S,
    *                                   I2S_MODE_LEFT_JUSTIFIED)
    */
@@ -168,7 +164,7 @@ typedef interface i2s_slave_callback_if {
  *  The component performs I2S slave so will expect the word clock and
  *  bit clock to be driven externally.
  *
- *  \param i              The I2S callback interface to connect to
+ *  \param i2s_i          The I2S callback interface to connect to
  *                        the application
  *  \param p_dout         An array of data output ports
  *  \param num_out        The number of output data ports
@@ -216,13 +212,13 @@ typedef interface tdm_callback_if {
    *   The I2S component will call this
    *   when it first initializes on first run of after a restart.
    *
-   *   \param mclk_bclk_ratio          This should be set to the desired master
-   *                                   clock to bit clock ratio(must be a power
-   *                                   of two thats greater than or equal to two
-   *                                   and less than or equal to 32.
-   *   \param mode                     The transfer mode (I2S_MODE_I2S,
-   *                                   I2S_MODE_LEFT_JUSTIFIED)
-   */
+   *   \param offset                   The offset in sclk clocks that the sync 
+   *                                   pulse should being at relative to the 
+   *                                   start of the data transfer.
+   *   \param sclk_edge_count          The number of sclk edges that the sync 
+   *                                   pulse should be high for.
+   *   \param channels_per_data_line   The number of channels per data line.
+   */ 
   void init(int &offset, unsigned &sclk_edge_count, unsigned &channels_per_data_line);//channels_per_data_line must be a power of two
 
   /**  I2S frame start callback.
