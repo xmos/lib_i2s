f0fbaae5ef0a5f2d7c507c3a8ac78a5347af3b63
Michael Bruno <mikeb@xmos.com>
2020-09-28 11:19:19 -0400

    Initial rewrite of I2S master in C
    I2S frame master has been converted to C and renamed as just I2S master.
    
    The I2S slave task has not yet been converted and the tests have not
    been updated.

--- lib_i2s/api/i2s.h
+++ lib_i2s/api/i2s.h
@@ -4,6 +4,10 @@
 #include <xs1.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <xcore/port.h>
+#include <xcore/clock.h>
+
+#define I2S_MAX_DATALINES 8
 
 /** I2S mode.
  *
@@ -35,16 +39,6 @@ typedef struct i2s_config_t {
   i2s_slave_bclk_polarity_t slave_bclk_polarity;  ///< Slave bit clock polarity.
 } i2s_config_t;
 
-/** TDM configuration structure.
- *
- *  This structure describes the configuration of a TDM bus.
- */
-typedef struct tdm_config_t {
-  int offset;               ///< The number of bits that the FSYNC signal transitions before the data. Must be a value between -31 and 31.
-  unsigned sync_len;        ///< The length that the FSYNC signal stays high counted as ticks of the bit clock.
-  unsigned channels_per_frame; ///< The number of channels in a TDM frame. This must be a power of 2.
-} tdm_config_t;
-
 /** Restart command type.
  *
  *  Restart commands that can be signalled to the I2S or TDM component.
@@ -54,6 +48,163 @@ typedef enum i2s_restart_t {
   I2S_RESTART,             ///< Restart the bus (causes the I2S/TDM to stop and a new init callback to occur allowing reconfiguration of the BUS).
   I2S_SHUTDOWN             ///< Shutdown. This will cause the I2S/TDM component to exit.
 } i2s_restart_t;
+
+#define I2S_CALLBACK_ATTR __attribute__((fptrgroup("i2s_callback")))
+
+/**
+ * I2S initialization event callback.
+ *
+ * The I2S component will call this
+ * when it first initializes on first run of after a restart.
+ *
+ * \param app_data    Points to application specific data supplied
+ *                    by the application. May be used for context
+ *                    data specific to each I2S task instance.
+ *
+ * \param i2s_config  This structure is provided if the connected
+ *                    component drives an I2S bus. The members
+ *                    of the structure should be set to the
+ *                    required configuration.
+ */
+typedef void (*i2s_init_t)(void *app_data, i2s_config_t *i2s_config);
+
+/**
+ * I2S restart check callback.
+ *
+ * This callback is called once per frame. The application must return the
+ * required restart behavior.
+ *
+ * \param app_data  Points to application specific data supplied
+ *                  by the application. May be used for context
+ *                  data specific to each I2S task instance.
+ *
+ * \return          The return value should be set to
+ *                  ``I2S_NO_RESTART``, ``I2S_RESTART`` or
+ *                  ``I2S_SHUTDOWN``.
+ */
+typedef i2s_restart_t (*i2s_restart_check_t)(void *app_data);
+
+/**
+ * Receive an incoming frame of samples.
+ *
+ * This callback will be called when a new frame of samples is read in by the I2S
+ * task.
+ *
+ * \param app_data  Points to application specific data supplied
+ *                  by the application. May be used for context
+ *                  data specific to each I2S task instance.
+ *
+ * \param num_in    The number of input channels contained within the array.
+ *
+ * \param samples   The samples data array as signed 32-bit values.  The component
+ *                  may not have 32-bits of accuracy (for example, many
+ *                  I2S codecs are 24-bit), in which case the bottom bits
+ *                  will be arbitrary values.
+ */
+typedef void (*i2s_receive_t)(void *app_data, size_t num_in, const int32_t *samples);
+
+/**
+ * Request an outgoing frame of samples.
+ *
+ * This callback will be called when the I2S task needs a new frame of samples.
+ *
+ * \param app_data  Points to application specific data supplied
+ *                  by the application. May be used for context
+ *                  data specific to each I2S task instance.
+ *
+ * \param num_out   The number of output channels contained within the array.
+ *
+ * \param samples   The samples data array as signed 32-bit values.  The component
+ *                  may not have 32-bits of accuracy (for example, many
+ *                  I2S codecs are 24-bit), in which case the bottom bits
+ *                  will be arbitrary values.
+ */
+typedef void (*i2s_send_t)(void *app_data, size_t num_out, int32_t *samples);
+
+/**
+ * Callback group representing callback events that can occur during the
+ * operation of the I2S task. Must be initialized by the application prior
+ * to passing it to one of the I2S tasks.
+ */
+typedef struct {
+    I2S_CALLBACK_ATTR i2s_init_t init; ///< Pointer to the init function.
+    I2S_CALLBACK_ATTR i2s_restart_check_t restart_check; ///< Pointer to the restart check function.
+    I2S_CALLBACK_ATTR i2s_receive_t receive; ///< Pointer to the receive function.
+    I2S_CALLBACK_ATTR i2s_send_t send; ///< Pointer to the send function.
+    void *app_data; ///< Pointer to application specific data which is passed to each callback.
+} i2s_callback_group_t;
+
+
+/** I2S master task
+ *
+ *  This task performs I2S on the provided pins. It will perform callbacks over
+ *  the i2s_callback_group_t callback group to get/receive frames of data from the
+ *  application using this component.
+ *
+ *  The task performs I2S master so will drive the word clock and
+ *  bit clock lines.
+ *
+ *  \param i2s_cbg        The I2S callback group pointing to the application's
+ *                        functions to use for initialization and getting and receiving
+ *                        frames. Also points to application specific data which will
+ *                        be shared between the callbacks.
+ *  \param p_dout         An array of data output ports
+ *  \param num_out        The number of output data ports
+ *  \param p_din          An array of data input ports
+ *  \param num_in         The number of input data ports
+ *  \param p_bclk         The bit clock output port
+ *  \param p_lrclk        The word clock output port
+ *  \param p_mclk         Input port which supplies the master clock
+ *  \param bclk           A clock that will get configured for use with
+ *                        the bit clock
+ */
+void i2s_master(
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_dout[],
+        const size_t num_out,
+        const port_t p_din[],
+        const size_t num_in,
+        const port_t p_bclk,
+        const port_t p_lrclk,
+        const port_t p_mclk,
+        const xclock_t bclk);
+
+/** I2S master task
+ *
+ *  This task differs from i2s_master() in that \p bclk must already be configured to
+ *  the BCLK frequency. Other than that, it is identical.
+ *
+ *  This task performs I2S on the provided pins. It will perform callbacks over
+ *  the i2s_callback_group_t callback group to get/receive frames of data from the
+ *  application using this component.
+ *
+ *  The task performs I2S master so will drive the word clock and
+ *  bit clock lines.
+ *
+ *  \param i2s_cbg        The I2S callback group pointing to the application's
+ *                        functions to use for initialization and getting and receiving
+ *                        frames. Also points to application specific data which will
+ *                        be shared between the callbacks.
+ *  \param p_dout         An array of data output ports
+ *  \param num_out        The number of output data ports
+ *  \param p_din          An array of data input ports
+ *  \param num_in         The number of input data ports
+ *  \param p_bclk         The bit clock output port
+ *  \param p_lrclk        The word clock output port
+ *  \param bclk           A clock that is configured externally to be used as the bit clock
+ *
+ */
+void i2s_master_external_clock(
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_dout[],
+        const size_t num_out,
+        const port_t p_din[],
+        const size_t num_in,
+        const port_t p_bclk,
+        const port_t p_lrclk,
+        const xclock_t bclk);
+
+#if 0
 
 /** Interface representing callback events that can occur during the
  *   operation of the I2S task
@@ -373,5 +524,7 @@ void tdm_master(client interface i2s_callback_if tdm_i,
 #include <i2s_slave_impl.h>
 #include <i2s_frame_slave_impl.h>
 #include <tdm_master_impl.h>
+
+#endif
 
 #endif // _i2s_h_
