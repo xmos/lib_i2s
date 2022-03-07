037e341538fa19ee192d7fdb5843d54cc4fb4c2f
Angel Cascarino <angelcascarino@xmos.com>
2022-03-07 17:55:54 +0000

    Updated copyright dates

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# Copyright 2015-2021 XMOS LIMITED.
+# Copyright 2015-2022 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 import xmostest
 from i2s_master_checker import I2SMasterChecker
