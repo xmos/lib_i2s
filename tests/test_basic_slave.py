0658b95d540332f3a54bce0d636272b6b3f93e02
Angel Cascarino <angelcascarino@xmos.com>
2022-09-05 16:30:47 +0100

    Ported basic_slave to pytest/pyxsim

--- tests/test_basic_slave.py
+++ tests/test_basic_slave.py
@@ -1,15 +1,24 @@
-# Copyright 2015-2021 XMOS LIMITED.
+# Copyright 2015-2022 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-import xmostest
 from i2s_slave_checker import I2SSlaveChecker
-from i2s_slave_checker import Clock
-import os
+from i2s_master_checker import Clock
+from pathlib import Path
+import Pyxsim
+import pytest
 
-def do_slave_test(num_in, num_out, testlevel):
+num_in_out_args = {"4ch_in,4ch_out": (4, 4),
+                   "1ch_in,1ch_out": (1, 1),
+                   "4ch_in,0ch_out": (4, 0),
+                   "0ch_in,4ch_out": (0, 4)}
 
-    resources = xmostest.request_resource("xsim")
+@pytest.mark.parametrize(("num_in", "num_out"), num_in_out_args.values(), ids=num_in_out_args.keys())
+def test_i2s_basic_slave(capfd, request, nightly, num_in, num_out):
+    testlevel = '0' if nightly else '1'
+    id_string = f"{num_in}_{num_out}"
+    id_string += "_smoke" if testlevel == '1' else ""
 
-    binary = 'i2s_slave_test/bin/{tl}_{i}{o}/i2s_slave_test_{tl}_{i}{o}.xe'.format(i=num_in, o=num_out, tl=testlevel)
+    cwd = Path(request.fspath).parent
+    binary = f'{cwd}/i2s_slave_test/bin/{id_string}/i2s_slave_test_{id_string}.xe'
 
     clk = Clock("tile[0]:XS1_PORT_1A")
 
@@ -21,26 +30,22 @@ def do_slave_test(num_in, num_out, testlevel):
         "tile[0]:XS1_PORT_1L",
         "tile[0]:XS1_PORT_16A",
         "tile[0]:XS1_PORT_1M",
-         clk)
+         clk,
+         False, # Don't check the bclk stops precisely as the hardware can't do that
+         False) # We're not running frame-based, so assume 32b data 
 
-    tester = xmostest.ComparisonTester(open('slave_test.expect'),
-                                     'lib_i2s', 'i2s_slave_sim_tests',
-                                     'basic_test_%s'%testlevel,
-                                     {'num_in':num_in, 'num_out':num_out},
-                                       regexp=True,
-                                       ignore=["CONFIG:.*"])
+    tester = Pyxsim.testers.AssertiveComparisonTester(
+        f'{cwd}/expected/slave_test.expect',
+        regexp = True,
+        ordered = True,
+        suppress_multidrive_messages=True,
+    )
 
-    tester.set_min_testlevel(testlevel)
-
-    xmostest.run_on_simulator(resources['xsim'], binary,
-                              simthreads = [clk, checker],
-                              simargs=[],
-                              #simargs=['--vcd-tracing', '-o ./i2s_slave_test/trace.vcd -tile tile[0] -ports-detailed'],
-                              suppress_multidrive_messages = True,
-                              tester = tester)
-
-def runtest():
-    do_slave_test(4, 4, "smoke")
-    do_slave_test(4, 0, "smoke")
-    do_slave_test(0, 4, "smoke")
-    do_slave_test(4, 4, "nightly")
+    Pyxsim.run_on_simulator(
+        binary,
+        tester=tester,
+        simthreads=[clk, checker],
+        build_env = {"NUMS_IN_OUT":f'{num_in};{num_out}', "SMOKE":testlevel},
+        simargs=[],
+        capfd=capfd
+    )
