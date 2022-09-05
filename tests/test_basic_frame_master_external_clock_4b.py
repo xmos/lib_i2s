485e0f7bcaf3a6223d76fb6fb81616e14d140cfa
Angel Cascarino <angelcascarino@xmos.com>
2022-09-05 15:14:05 +0100

    Port external_clock_4b to pytest/pyxsim

--- tests/test_basic_frame_master_external_clock_4b.py
+++ tests/test_basic_frame_master_external_clock_4b.py
@@ -1,74 +1,60 @@
 # Copyright 2015-2022 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-import xmostest
-from i2s_master_checker import I2SMasterChecker
-from i2s_master_checker import Clock
-import os
+from i2s_master_checker import I2SMasterChecker, Clock
+from pathlib import Path
+import Pyxsim
+import pytest
 
+num_in_out_args = {"4ch_in,4ch_out": (4, 4),
+                   "1ch_in,1ch_out": (1, 1),
+                   "4ch_in,0ch_out": (4, 0),
+                   "0ch_in,4ch_out": (0, 4)}
 
-def do_master_test(data_bits, num_in, num_out, testlevel):
+bitdepth_args = {"8b": 8,
+                 "16b": 16,
+                 "24b": 24,
+                 "32b": 32}
 
-    resources = xmostest.request_resource("xsim")
+@pytest.mark.parametrize("bitdepth", bitdepth_args.values(), ids=bitdepth_args.keys())
+@pytest.mark.parametrize(("num_in", "num_out"), num_in_out_args.values(), ids=num_in_out_args.keys())
+def test_i2s_basic_master_4b(capfd, request, nightly, bitdepth, num_in, num_out):
+    testlevel = '0' if nightly else '1'
+    id_string = f"{bitdepth}_{num_in}_{num_out}"
+    id_string += "_smoke" if testlevel == '1' else ""
 
-    id_string = "{tl}_{db}{i}{o}".format(
-        db=data_bits, i=num_in, o=num_out, tl=testlevel
-    )
-
-    binary = "i2s_frame_master_external_clock_4b_test/bin/{id}/i2s_frame_master_external_clock_4b_test_{id}.xe".format(
-        id=id_string
-    )
+    cwd = Path(request.fspath).parent
+    binary = f'{cwd}/i2s_frame_master_external_clock_4b_test/bin/{id_string}/i2s_frame_master_external_clock_4b_test_{id_string}.xe'
 
     clk = Clock("tile[0]:XS1_PORT_1A")
 
     checker = I2SMasterChecker(
         "tile[0]:XS1_PORT_1B",
         "tile[0]:XS1_PORT_1C",
-        [
-            "tile[0]:XS1_PORT_4F.3",
-            "tile[0]:XS1_PORT_4F.2",
-            "tile[0]:XS1_PORT_4F.1",
-            "tile[0]:XS1_PORT_4F.0",
-        ],
-        [
-            "tile[0]:XS1_PORT_4E.3",
-            "tile[0]:XS1_PORT_4E.2",
-            "tile[0]:XS1_PORT_4E.1",
-            "tile[0]:XS1_PORT_4E.0",
-        ],
+        ["tile[0]:XS1_PORT_4F.3","tile[0]:XS1_PORT_4F.2","tile[0]:XS1_PORT_4F.1", "tile[0]:XS1_PORT_4F.0"],
+        ["tile[0]:XS1_PORT_4E.3","tile[0]:XS1_PORT_4E.2","tile[0]:XS1_PORT_4E.1", "tile[0]:XS1_PORT_4E.0"],
         "tile[0]:XS1_PORT_1L",
         "tile[0]:XS1_PORT_16A",
         "tile[0]:XS1_PORT_1M",
-        clk,
-        False,  # Don't check the bclk stops precisely as the hardware can't do that
-        True,  # We're running the frame-based master, so can have variable data widths
-    )
+         clk,
+         False, # Don't check the bclk stops precisely as the hardware can't do that
+         True)  # We're running the frame-based master, so can have variable data widths
 
-    tester = xmostest.ComparisonTester(
-        open("master_test.expect"),
-        "lib_i2s",
-        "i2s_frame_master_4b_sim_tests",
-        "basic_test_%s" % testlevel,
-        {"data_bits": data_bits, "num_in": num_in, "num_out": num_out},
-        ignore=["CONFIG:.*"],
-    )
+    binary = f"{cwd}/i2s_frame_master_external_clock_4b_test/bin/{id_string}/i2s_frame_master_external_clock_4b_test_{id_string}.xe"
 
-    tester.set_min_testlevel(testlevel)
+    clk = Clock("tile[0]:XS1_PORT_1A")
 
-    xmostest.run_on_simulator(
-        resources["xsim"],
-        binary,
-        simthreads=[clk, checker],
-        simargs=[],
-        # simargs=['--trace-to', './i2s_frame_master_external_clock_test/logs/sim_{id}.log'.format(id=id_string),
-        #         '--vcd-tracing', '-o ./i2s_frame_master_external_clock_test/traces/trace_{id}.vcd -tile tile[0] -ports-detailed -functions -cycles -clock-blocks -cores -instructions'.format(id=id_string)],
+    tester = Pyxsim.testers.AssertiveComparisonTester(
+        f'{cwd}/expected/master_test.expect',
+        regexp = True,
+        ordered = True,
         suppress_multidrive_messages=True,
-        tester=tester,
     )
 
-
-def runtest():
-    do_master_test(32, 4, 4, "smoke")
-    do_master_test(32, 1, 1, "smoke")
-    do_master_test(32, 4, 0, "smoke")
-    do_master_test(32, 0, 4, "smoke")
-    do_master_test(32, 4, 4, "nightly")
+    Pyxsim.run_on_simulator(
+        binary,
+        tester=tester,
+        simthreads=[clk, checker],
+        build_env = {"BITDEPTHS":f"{bitdepth}", "NUMS_IN_OUT":f'{num_in};{num_out}', "SMOKE":testlevel},
+        simargs=[],
+        capfd=capfd
+    )
