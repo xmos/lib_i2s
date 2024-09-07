d772587f61dd7aa0a2c16bd5cc66c60f4d678d8e
Ed <ed@xmos.com>
2024-09-07 18:06:49 +0100

    WIP backpressure test python

--- tests/test_backpressure.py
+++ tests/test_backpressure.py
@@ -4,22 +4,16 @@ import pytest
 import Pyxsim
 from Pyxsim.pyxsim import XsiLoopbackPlugin
 from pathlib import Path
+import json
 
-sample_rate_args = {"384kbps": 384000,
-                    "192kbps": 192000}
+with open(Path(__file__).parent / "backpressure_test/test_params.json") as f:
+    params = json.load(f)
 
-num_channels_args = {"1ch": 1,
-                     "2ch": 2,
-                     "3ch": 3,
-                     "4ch": 4}
-
-rx_tx_inc_args = {"rx_delay_inc_50ns,tx_delay_inc_50ns": (5, 5),
-                  "rx_delay_inc_0ns,tx_delay_inc_100ns": (0, 10),
-                  "rx_delay_inc_100ns,tx_delay_inc_0ns": (10, 0)}
-
-bitdepth_args = {"8b": 8,
-                 "16b": 16,
-                 "32b": 32}
+rx_tx_inc_args = {}
+for item in params["RX_TX_INCS"]:
+    rx = item["RX"]
+    tx = item["TX"]
+    rx_tx_inc_args[f"rx_delay_inc_{rx*10}ns,tx_delay_inc_{tx*10}ns"] = [rx, tx]
 
 # 384000, num_channels 4, bitdepths 8 and 16 have zero backpressure so skip the test
 def uncollect_if(bitdepth, sample_rate, num_channels, receive_increment, send_increment):
@@ -27,16 +21,16 @@ def uncollect_if(bitdepth, sample_rate, num_channels, receive_increment, send_in
         return True
 
 @pytest.mark.uncollect_if(func=uncollect_if)
-@pytest.mark.parametrize("bitdepth", bitdepth_args.values(), ids=bitdepth_args.keys())
-@pytest.mark.parametrize("sample_rate", sample_rate_args.values(), ids=sample_rate_args.keys())
-@pytest.mark.parametrize("num_channels", num_channels_args.values(), ids=num_channels_args.keys())
+@pytest.mark.parametrize("bitdepth", params["BITDEPTHS"], ids=[f"{bd}b" for bd in params["BITDEPTHS"]])
+@pytest.mark.parametrize("sample_rate", params["SAMPLE_RATES"], ids=[f"{sr}Hz" for sr in params["SAMPLE_RATES"]])
+@pytest.mark.parametrize("num_channels", params["I2S_LINES"])
 @pytest.mark.parametrize(("receive_increment", "send_increment"), rx_tx_inc_args.values(), ids=rx_tx_inc_args.keys())
 def test_backpressure(nightly, capfd, request, sample_rate, num_channels, receive_increment, send_increment, bitdepth):
     id_string = f"{bitdepth}_{sample_rate}_{num_channels}_{receive_increment}_{send_increment}"
 
     cwd = Path(request.fspath).parent
 
-    binary = f'{cwd}/backpressure_test/bin/{id_string}/backpressure_test_{id_string}.xe'
+    binary = f'{cwd}/backpressure_test/bin/test_i2s_backpressure_{bitdepth}_{sample_rate}_{num_channels}_{receive_increment}_{send_increment}.xe'
 
     loopback = XsiLoopbackPlugin(tile="tile[0]", from_port="XS1_PORT_1G", to_port="XS1_PORT_1A")
 
@@ -46,10 +40,10 @@ def test_backpressure(nightly, capfd, request, sample_rate, num_channels, receiv
                                                     suppress_multidrive_messages=True,
                                                     ignore=["CONFIG:.*"])
 
-    Pyxsim.run_on_simulator(
+    Pyxsim.run_on_simulator_(
         binary,
         tester=tester,
-        build_env = {"BITDEPTHS":bitdepth, "SAMPLE_RATES":sample_rate, "CHANS":num_channels, "RX_TX_INCS":f"{receive_increment};{send_increment}"},
+        do_xe_prebuild=False,
         simargs=[],
         capfd=capfd,
         plugins=[loopback]
